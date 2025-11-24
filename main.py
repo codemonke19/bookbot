d0d8697b56d3cd3f012eb481ec7d0b45277d4e79
codemonke19 <codemonke@proton.me>
2025-11-24 15:42:31 -0500

    Added __pycache__ to .gitignore Added function to count characters to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_characters
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,6 +7,9 @@ def get_book_text(path_to_file):
 
 def main(path_to_file):
     book_text = get_book_text(path_to_file)
-    count_words(book_text)
+    num_words = count_words(book_text)
+    char_count = count_characters(book_text)
+    print(f"Found {num_words} total words")
+    print(char_count)
 
 main("./books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -3,4 +3,14 @@ def count_words(book_text):
     num_words = 0
     for word in words:
         num_words += 1
-    print(f"Found {num_words} total words")
+    return num_words
+
+def count_characters(book_text):
+    char_count = {}
+    small_text = book_text.lower()
+    for char in small_text:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+    return char_count
