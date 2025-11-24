2072739a5a916f34cde3e741e53372de3851784d
codemonke19 <codemonke@proton.me>
2025-11-23 20:22:55 -0500

    counting words

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ def get_book_text(path_to_file):
 
 def main(path_to_file):
     book_text = get_book_text(path_to_file)
-    print(book_text)
+    return book_text
 
-main("./books/frankenstein.txt")
+def count_words(path_to_file):
+    text = main(path_to_file)
+    words = text.split()
+    num_words = 0
+    for word in words:
+        num_words += 1
+    print(f"Found {num_words} total words")
+
+count_words("./books/frankenstein.txt")
